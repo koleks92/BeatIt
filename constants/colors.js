5d5fbd76d93b5d405c701d42ab7a6ee5f909f53f
koleks92 <koleks92@gmail.com>
2023-12-06 21:34:42 +0100

    Colors constant

---
+++ constants/colors.js
@@ -0,0 +1,15 @@
+// Constant colors for app
+
+export const Colors = {
+    background: '#001F3F',
+    background2: '#6A1B9A',
+    text: '#FFFFFF',
+    text2: '#F5F5F5',
+    highlight: '#30D5C8',
+    highlight2: '#FF00FF',
+    accent: '#0074D9',
+    accent2: '#FF4136',
+    record: '#FF4136',
+    pad: '#D3D3D3',
+    padPressed: '#a9a9a9',    
+};
