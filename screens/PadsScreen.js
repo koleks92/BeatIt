677195eb5d961ebf834c0c852c000ca325843543
koleks92 <koleks92@gmail.com>
2023-12-17 20:58:49 +0100

    Template for screens

---
+++ screens/PadsScreen.js
@@ -0,0 +1,14 @@
+// Shows pads/mpc sceeen
+
+import { Text } from "react-native";
+import Background from "../components/UI/Background";
+
+function PadsScreen() {
+  return (
+    <Background>
+      <Text>Pads</Text>
+    </Background>
+  );
+}
+
+export default PadsScreen;
---
+++ screens/PianoScreen.js
@@ -0,0 +1,14 @@
+// Shows piano sceeen
+
+import { Text } from "react-native";
+import Background from "../components/UI/Background";
+
+function PianoScreen() {
+  return (
+    <Background>
+      <Text>Piano</Text>
+    </Background>
+  );
+}
+
+export default PianoScreen;
