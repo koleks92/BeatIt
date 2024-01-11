3abd3d00e5a10e0a766ad87d2ed69b158f0f3a29
koleks92 <koleks92@gmail.com>
2024-01-11 20:47:41 +0100

    LinearGradient

--- components/UI/Background.js
+++ components/UI/Background.js
@@ -1,31 +1,36 @@
 import { ImageBackground, StyleSheet, View } from "react-native";
 import { Colors } from "../../constants/colors";
+import { LinearGradient } from "expo-linear-gradient";
 
 function Background({ children }) {
-    return (
-        <View style={styles.root}>
-          <ImageBackground
-            source={require("../../assets/background.jpg")}
-            resizeMode='cover'
-            style={styles.root}
-            imageStyle={styles.backgroundImage}
-          >
-            { children }
-          </ImageBackground>
-        </View>
-      );
+  return (
+    <View style={styles.root}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={[Colors.background, Colors.bar]}
+        style={styles.root}
+      >
+        <ImageBackground
+          source={require("../../assets/background.jpg")}
+          resizeMode="cover"
+          style={styles.root}
+          imageStyle={styles.backgroundImage}
+        >
+          {children}
+        </ImageBackground>
+      </LinearGradient>
+    </View>
+  );
 }
 
 export default Background;
 
 const styles = StyleSheet.create({
-    root: {
-      flex: 1,
-      backgroundColor: Colors.background,
-      opacity: 0.8
-    },
-    backgroundImage: {
-      flex: 1,
-      opacity: 0.5
-    }
-  });
+  root: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    opacity: 0.25,
+  },
+});
