0d00e35662122aab72bf07bdb78b96bdf5270711
koleks92 <koleks92@gmail.com>
2024-02-17 19:50:36 +0100

    Load audio files

--- App.js
+++ App.js
@@ -8,10 +8,38 @@ import PadsScreen from "./screens/PadsScreen";
 import PianoScreen from "./screens/PianoScreen";
 import { Colors } from "./constants/colors";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Audio } from 'expo-av'; 
 
 const Tab = createBottomTabNavigator();
 const scrW = Dimensions.get("window").width;
 const scrH = Dimensions.get("window").height;
+const NUM_PADS = 15;
+const padsFiles = Array.from({ length: NUM_PADS }, (_, index) => new Audio.Sound());
+
+async function loadAudioFiles() {
+  try {
+
+    await padsFiles[0].loadAsync(require('./sounds/pads/bass_1.mp3'))
+    await padsFiles[1].loadAsync(require('./sounds/pads/bass_2.mp3'))
+    await padsFiles[2].loadAsync(require('./sounds/pads/bass_3.mp3'))
+    await padsFiles[3].loadAsync(require('./sounds/pads/clap_1.mp3'))
+    await padsFiles[4].loadAsync(require('./sounds/pads/clap_2.mp3'))
+    await padsFiles[5].loadAsync(require('./sounds/pads/clap_3.mp3'))
+    await padsFiles[6].loadAsync(require('./sounds/pads/hihat_1.mp3'))
+    await padsFiles[7].loadAsync(require('./sounds/pads/hihat_2.mp3'))
+    await padsFiles[8].loadAsync(require('./sounds/pads/hihat_3.mp3'))
+    await padsFiles[9].loadAsync(require('./sounds/pads/kick_1.mp3'))
+    await padsFiles[10].loadAsync(require('./sounds/pads/kick_2.mp3'))
+    await padsFiles[11].loadAsync(require('./sounds/pads/kick_3.mp3'))
+    await padsFiles[12].loadAsync(require('./sounds/pads/snare_1.mp3'))
+    await padsFiles[13].loadAsync(require('./sounds/pads/snare_1.mp3'))
+    await padsFiles[14].loadAsync(require('./sounds/pads/snare_1.mp3'))
+
+    console.log("Sucessfull sounds loading!")
+  } catch (error) {
+    console.error('Error loading', error)
+  }
+}
 
 function Tabs() {
   return (
@@ -49,6 +77,7 @@ function Tabs() {
       <Tab.Screen
         name="PADS"
         component={PadsScreen}
+        initialParams={{ padsFiles }}
         options={{
           headerShown: false, // Hide the top label for this screen
         }}
@@ -71,7 +100,7 @@ export default function App() {
   useEffect(() => {
     async function prepare() {
       try {
-        console.log("SplashScreen");
+        loadAudioFiles();
       } catch (e) {
         console.warn(e);
       } finally {
