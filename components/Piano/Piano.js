e5a73bd0594033435d3636ab3fe0bc483633ee81
koleks92 <koleks92@gmail.com>
2024-02-19 19:34:09 +0100

    One octave of keys - not fully functional

--- components/Piano/Piano.js
+++ components/Piano/Piano.js
@@ -1,28 +1,99 @@
-import { StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import White from "./White";
+import Black from "./Black";
+
+const scrW = Dimensions.get("window").width;
 
 function Piano() {
-    return (
+  return (
     <View style={styles.root}>
-       <White onBegin={() => console.log("Begin")} onEnd={() => console.log("End")}/>
-       <White onBegin={() => console.log("Begin")} onEnd={() => console.log("End")}/>
-       <White onBegin={() => console.log("Begin")} onEnd={() => console.log("End")}/>
-       <White onBegin={() => console.log("Begin")} onEnd={() => console.log("End")}/>
-       <White onBegin={() => console.log("Begin")} onEnd={() => console.log("End")}/>
-       <White onBegin={() => console.log("Begin")} onEnd={() => console.log("End")}/>
-       <White onBegin={() => console.log("Begin")} onEnd={() => console.log("End")}/>
+      <View style={styles.whiteBlackContainer}>
+        <White
+          onBegin={() => console.log("Begin")}
+          onEnd={() => console.log("End")}
+        />
+        <View style={styles.blackKeyContainer}>
+          <Black
+            onBegin={() => console.log("Begin")}
+            onEnd={() => console.log("End")}
+          />
+        </View>
+      </View>
+      <View >
+        <View style={styles.blackKeyContainer}>
+          <Black
+            onBegin={() => console.log("Begin")}
+            onEnd={() => console.log("End")}
+          />
+        </View>
+        <White
+          onBegin={() => console.log("Begin")}
+          onEnd={() => console.log("End")}
+        />
+      </View>
+      <White
+        onBegin={() => console.log("Begin")}
+        onEnd={() => console.log("End")}
+      />
+      <View>
+        <View style={styles.blackKeyContainer}>
+          <Black
+            onBegin={() => console.log("Begin")}
+            onEnd={() => console.log("End")}
+          />
+        </View>
+        <White
+          onBegin={() => console.log("Begin")}
+          onEnd={() => console.log("End")}
+        />
+      </View>
+      <View>
+        <View style={styles.blackKeyContainer}>
+          <Black
+            onBegin={() => console.log("Begin")}
+            onEnd={() => console.log("End")}
+          />
+        </View>
+        <White
+          onBegin={() => console.log("Begin")}
+          onEnd={() => console.log("End")}
+        />
+      </View>
+      <View>
+        <View style={styles.blackKeyContainer}>
+          <Black
+            onBegin={() => console.log("Begin")}
+            onEnd={() => console.log("End")}
+          />
+        </View>
+        <White
+          onBegin={() => console.log("Begin")}
+          onEnd={() => console.log("End")}
+        />
+      </View>
+      <White
+        onBegin={() => console.log("Begin")}
+        onEnd={() => console.log("End")}
+      />
     </View>
-
-    )
-};
+  );
+}
 
 export default Piano;
 
 const styles = StyleSheet.create({
-    root: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
-
+  root: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  blackKeyContainer: {
+    position: "absolute",
+    left: "30%",
+    top: "75%",
+    zIndex: 2,
+  },
+  whiteBlackContainer: {
+    zIndex: 1,
+  },
+});
