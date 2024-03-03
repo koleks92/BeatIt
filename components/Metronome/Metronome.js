b8ee69d2f3fc729fd11e76cda3e96f7c9cc5f86d
koleks92 <koleks92@gmail.com>
2024-03-03 18:57:32 +0100

    Metronome functionality added

--- components/Metronome/Metronome.js
+++ components/Metronome/Metronome.js
@@ -1,10 +1,80 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { View, Text, Modal, Pressable, StyleSheet } from "react-native";
 import { Colors } from "../../constants/colors";
+import { Audio } from "expo-av";
 
 function Metronome() {
     const [modalVisible, setModalVisible] = useState(false);
+    const [BPM, setBPM] = useState(120);
+    const metronome = useRef(null);
+    const onMinusRef = useRef(null);
+    const onPlusRef = useRef(null);
+    const [metronomeOn, setMetronomeOn] = useState(false);
 
+    let sound = new Audio.Sound();
+    // Load sounds
+    async function loadSound() {
+        await sound.loadAsync(require("../../sounds/metronome/metronome.mp3"));
+    }
+
+    loadSound();
+
+    // Minus button functionality
+    const onMinusIn = () => {
+        if (BPM === 1) {
+        } else {
+            onMinusRef.current = setInterval(() => {
+                setBPM((prevBPM) => prevBPM - 1);
+            }, 100);
+        }
+    };
+
+    const onMinusOut = () => {
+        clearInterval(onMinusRef.current);
+    };
+
+    // Plus button functionality
+    const onPlusIn = () => {
+        if (BPM === 240) {
+        } else {
+            onPlusRef.current = setInterval(() => {
+                setBPM((prevBPM) => prevBPM + 1);
+            }, 100);
+        }
+    };
+
+    const onPlusOut = () => {
+        clearInterval(onPlusRef.current);
+    };
+
+    // Start metronome functionality
+    const onBegin = () => {
+        setMetronomeOn(true);
+        const intervalInMilliSeconds = (60 / BPM) * 1000;
+        metronome.current = setInterval(async () => {
+            await sound.replayAsync();
+        }, intervalInMilliSeconds);
+    };
+
+    // End metronome functionality
+    const onEnd = () => {
+        clearInterval(metronome.current);
+        setMetronomeOn(false);
+    };
+
+    // Handling BPM values
+    useEffect(() => {
+        onEnd();
+        if (BPM >= 240) {
+            clearInterval(onPlusRef.current);
+            setBPM(240);
+        } else if (BPM <= 1) {
+            clearInterval(onMinusRef.current);
+            setBPM(1);
+        }
+    }, [BPM]);
+
+    // Modal functionality
     const openModal = () => {
         setModalVisible(true);
     };
@@ -23,8 +93,44 @@ function Metronome() {
             >
                 <View style={styles.modalContainer}>
                     <View style={styles.metronomeContainer}>
-                        <Text style={styles.text}>Metronome</Text>
-                        <Pressable style={[styles.buttonClose]} onPress={closeModal}>
+                        <View style={styles.buttons}>
+                            <Pressable
+                                style={styles.button}
+                                onPressIn={onMinusIn}
+                                onPressOut={onMinusOut}
+                            >
+                                <Text>-</Text>
+                            </Pressable>
+                            <Text>{BPM}</Text>
+                            <Pressable
+                                style={styles.button}
+                                onPressIn={onPlusIn}
+                                onPressOut={onPlusOut}
+                            >
+                                <Text>+</Text>
+                            </Pressable>
+                        </View>
+                        <View style={styles.buttons}>
+                            <Pressable
+                                style={[
+                                    styles.button,
+                                    metronomeOn && styles.buttonOn,
+                                ]}
+                                onPress={() => onBegin()}
+                            >
+                                <Text>Start</Text>
+                            </Pressable>
+                            <Pressable
+                                style={styles.button}
+                                onPress={() => onEnd()}
+                            >
+                                <Text>End</Text>
+                            </Pressable>
+                        </View>
+                        <Pressable
+                            style={[styles.buttonClose]}
+                            onPress={closeModal}
+                        >
                             <Text>Close</Text>
                         </Pressable>
                     </View>
@@ -62,7 +168,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 4,
         elevation: 5,
-        opacity: 0.9
+        opacity: 0.9,
     },
     text: {
         color: Colors.text2,
@@ -71,6 +177,21 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         padding: 10,
         elevation: 2,
-        backgroundColor: Colors.icon
+        backgroundColor: Colors.icon,
+    },
+    buttons: {
+        flexDirection: "row",
+        alignContent: "space-around",
+    },
+    button: {
+        width: 100,
+        height: 100,
+        backgroundColor: "lightblue",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonOn: {
+        borderWidth: 1,
+        borderColor: "black",
     },
 });
