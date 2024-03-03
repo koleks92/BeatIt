ff67bb2662b0b88f9359f37669f90500c6317632
koleks92 <koleks92@gmail.com>
2024-03-03 18:48:39 +0100

    Box, text styling

--- components/Metronome/Metronome.js
+++ components/Metronome/Metronome.js
@@ -1,5 +1,6 @@
-import { useRef, useEffect, useState } from "react";
-import { View, Text, Modal, Button } from "react-native";
+import { useState } from "react";
+import { View, Text, Modal, Pressable, StyleSheet } from "react-native";
+import { Colors } from "../../constants/colors";
 
 function Metronome() {
     const [modalVisible, setModalVisible] = useState(false);
@@ -16,20 +17,60 @@ function Metronome() {
         <>
             <Modal
                 animationType="fade"
-                transparent={false}
+                transparent={true}
                 visible={modalVisible}
                 onRequestClose={closeModal}
             >
-                <View>
-                    <Text>Metronome</Text>
-                    <Button title="Close" onPress={closeModal} />
+                <View style={styles.modalContainer}>
+                    <View style={styles.metronomeContainer}>
+                        <Text style={styles.text}>Metronome</Text>
+                        <Pressable style={[styles.buttonClose]} onPress={closeModal}>
+                            <Text>Close</Text>
+                        </Pressable>
+                    </View>
                 </View>
             </Modal>
             <View>
-                <Button title="Metronome" onPress={openModal} />
+                <Pressable onPress={openModal}>
+                    <Text>Metronome</Text>
+                </Pressable>
             </View>
         </>
     );
 }
 
 export default Metronome;
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    metronomeContainer: {
+        margin: 20,
+        backgroundColor: Colors.bar,
+        color: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        opacity: 0.9
+    },
+    text: {
+        color: Colors.text2,
+    },
+    buttonClose: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        backgroundColor: Colors.icon
+    },
+});
