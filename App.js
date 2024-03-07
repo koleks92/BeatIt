659a11470d8f3ee3c4ffdbdfaa12d2a5b5903bc4
koleks92 <koleks92@gmail.com>
2024-03-07 12:55:29 +0100

    SplashScreen fix

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Image, Dimensions } from "react-native";
+import { StyleSheet, Image, Dimensions, View } from "react-native";
 import React, { useCallback, useEffect, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -15,97 +15,99 @@ const scrW = Dimensions.get("window").width;
 const scrH = Dimensions.get("window").height;
 
 function Tabs() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color }) => {
-          let iconName;
-          if (route.name === "PADS") {
-            iconName = require("./assets/icons/pads.png");
-          } else if (route.name === "PIANO") {
-            iconName = require("./assets/icons/piano.png");
-          }
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color }) => {
+                    let iconName;
+                    if (route.name === "PADS") {
+                        iconName = require("./assets/icons/pads.png");
+                    } else if (route.name === "PIANO") {
+                        iconName = require("./assets/icons/piano.png");
+                    }
 
-          return (
-            <Image
-              source={iconName}
-              style={{
-                width: scrH * 0.05,
-                height: scrH * 0.05,
-                tintColor: color,
-              }}
+                    return (
+                        <Image
+                            source={iconName}
+                            style={{
+                                width: scrH * 0.05,
+                                height: scrH * 0.05,
+                                tintColor: color,
+                            }}
+                        />
+                    );
+                },
+                tabBarActiveTintColor: Colors.text,
+                tabBarActiveBackgroundColor: Colors.background,
+                tabBarLabel: () => null, // Hide labels by returning null
+                tabBarStyle: {
+                    backgroundColor: Colors.bar,
+                    height: Platform.OS === "ios" ? scrH * 0.1 : scrH * 0.07,
+                    borderTopWidth: 0,
+                },
+            })}
+        >
+            <Tab.Screen
+                name="PADS"
+                component={PadsScreen}
+                options={{
+                    headerShown: false, // Hide the top label for this screen
+                }}
             />
-          );
-        },
-        tabBarActiveTintColor: Colors.text,
-        tabBarActiveBackgroundColor: Colors.background,
-        tabBarLabel: () => null, // Hide labels by returning null
-        tabBarStyle: {
-          backgroundColor: Colors.bar,
-          height: Platform.OS === "ios" ? scrH * 0.1 : scrH * 0.07,
-          borderTopWidth: 0,
-        },
-      })}
-    >
-      <Tab.Screen
-        name="PADS"
-        component={PadsScreen}
-        options={{
-          headerShown: false, // Hide the top label for this screen
-        }}
-      />
-      <Tab.Screen
-        name="PIANO"
-        component={PianoScreen}
-        options={{
-          headerShown: false, // Hide the top label for this screen
-        }}
-      />
-    </Tab.Navigator>
-  );
+            <Tab.Screen
+                name="PIANO"
+                component={PianoScreen}
+                options={{
+                    headerShown: false, // Hide the top label for this screen
+                }}
+            />
+        </Tab.Navigator>
+    );
 }
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
+    const [appIsReady, setAppIsReady] = useState(false);
 
-  // Splash screen
-  useEffect(() => {
-    async function prepare() {
-      try {
-        console.log("Loading...");
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
+    useEffect(() => {
+      async function prepare() {
+          try {
+              await new Promise((resolve) => setTimeout(resolve, 2000));
+          } catch (e) {
+              console.warn(e);
+          } finally {
+              setAppIsReady(true);
+          }
       }
-    }
-    prepare();
+      prepare();
   }, []);
 
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
+  useEffect(() => {
+      async function hideSplashScreen() {
+          if (appIsReady) {
+              await SplashScreen.hideAsync();
+          }
+      }
+      hideSplashScreen();
   }, [appIsReady]);
 
   if (!appIsReady) {
-    return null;
+      return null;
   }
 
-  return (
-    <SoundsContextProvider>
-      <StatusBar style="dark" />
-          <SafeAreaProvider>
-            <NavigationContainer>
-              <Tabs />
-            </NavigationContainer>
-          </SafeAreaProvider>
-    </SoundsContextProvider>
-  );
+    return (
+            <SoundsContextProvider>
+                <StatusBar style="dark" />
+                <SafeAreaProvider>
+                    <NavigationContainer>
+                        <Tabs />
+                    </NavigationContainer>
+                </SafeAreaProvider>
+            </SoundsContextProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
