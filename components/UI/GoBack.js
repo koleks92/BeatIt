001a0f432124c9b4a316d0d501e9a03b55c47564
koleks92 <koleks92@gmail.com>
2024-03-21 21:25:57 +0100

    GoBack button

---
+++ components/UI/GoBack.js
@@ -0,0 +1,26 @@
+import { Pressable, StyleSheet, Dimensions, Image } from "react-native";
+
+
+function GoBack({iconSize, onPress}) {
+
+    return (
+        <Pressable onPress={onPress} style={styles.iconContainer}>
+            <Image
+                source={require("../../assets/icons/goback.png")}
+                resizeMode="contain"
+                style={{ height: iconSize }}
+            />
+        </Pressable>
+    );
+}
+
+export default GoBack;
+
+const styles = StyleSheet.create({
+    iconContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+    },
+});
