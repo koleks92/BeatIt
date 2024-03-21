1f54256322ba5153d4804b5099a6654624d7421c
koleks92 <koleks92@gmail.com>
2024-03-21 20:50:11 +0100

    Pads changer icon

---
+++ components/UI/PadsChanger.js
@@ -0,0 +1,77 @@
+import { Pressable, StyleSheet, Dimensions, Image } from "react-native";
+import { Colors } from "../../constants/colors";
+
+const scrW = Dimensions.get("window").width;
+
+function PadsChanger({iconSize}) {
+    function openSomething() {
+        console.log("Pressed");
+    }
+    return (
+        <Pressable onPress={openSomething} style={styles.iconContainer}>
+            <Image
+                source={require("../../assets/icons/edit.png")}
+                resizeMode="contain"
+                style={{ height: iconSize }}
+            />
+        </Pressable>
+    );
+}
+
+export default PadsChanger;
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    metronomeContainer: {
+        marginHorizontal: scrW * 0.1,
+        marginVertical: scrW * 0.6,
+        backgroundColor: Colors.bar,
+        color: "white",
+        borderRadius: 20,
+        padding: scrW * 0.05,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    title: {
+        color: Colors.text2,
+        fontSize: scrW * 0.07,
+    },
+    text: {
+        color: Colors.text2,
+        fontSize: scrW * 0.06,
+    },
+    bpmView: {
+        justifyContent: "center",
+        alignItems: "center",
+        margin: scrW * 0.05,
+        width: scrW * 0.12,
+    },
+    buttons: {
+        flexDirection: "row",
+        flex: 1,
+    },
+    buttonOn: {
+        borderWidth: 3,
+        borderColor: Colors.accent2,
+    },
+    buttonView: {
+        flex: 1,
+    },
+    iconContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center",
+    },
+});
